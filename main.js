229865465fafd2cd5fdb66332a5403f080f2fea2
Tobias André Arildset <soulkake@soulnetwork.eu>
2019-07-17 16:17:33 +0200

    Update system

--- main.js
+++ main.js
@@ -1,28 +1,49 @@
-var { app, BrowserWindow, ipcMain } = require('electron');
-var mysql = require('mysql');
-var url = require('url');
-var path = require('path');
-let maxversion;
+const { app, BrowserWindow, ipcMain } = require('electron');
+const electronLogger = require('electron-log');
+const child_process = require('child_process');
+const mysql = require('mysql');
+const url = require('url');
+const path = require('path');
+const http = require('http');
+const fs = require('fs');
+const zlib = require('zlib');
+const unzipper = require('unzipper');
+let maxversion = null;
 
 const { autoUpdater } = require('electron-updater');
+/*const connection = new mysql.createConnection({
+    host            : "nl1-wss3.a2hosting.com",
+    user            : "souln_soulslnchr",
+    password        : "Xgmu96^8",
+    database        : "soulnetw_users_kae",
+    port            : 3306,
+});*/
+
 const connection = new mysql.createConnection({
-    host            : "localhost",
-    user            : "root",
-    password        : "",
+    host            : "90.149.113.127",
+    user            : "readonly",
+    password        : "NVtMq31GJCeEq8Ax",
     database        : "soulslauncher",
-    port            : 3306
+    port            : 15603,
 });
 
 connection.connect();
 
 //maxversionnumber = connection.query('SELECT MAX(versionnumber) FROM soulslauncher');
 
-connection.query('SELECT version FROM `soulslauncher` ORDER BY version DESC', function(error, results, fields) {
+connection.query('SELECT version FROM `soulslauncher` ORDER BY version DESC', function(error, results) {
     if(error) throw error;
-    maxversion = results[0];
+    maxversion = results[0]['version']/*.toString()*/;
 });
 
-const currentversion = 'a0.0.1';
+connection.query('SELECT versionnumber FROM `soulslauncher` ORDER BY version DESC', function(error, results, fields) {
+    if(error) throw error;
+    maxversionnumber = results[0]['versionnumber'];
+});
+
+const currentversion = 'a0.0.3';
+global.currentVersion = currentversion;
+const currentversionnumber = '3';
 let win = null;
 let splash = null;
 let newVersion = null;
@@ -32,10 +53,15 @@ app.on('ready', () => {
     win = new BrowserWindow({ width: 1005, height: 600, icon: path.join(__dirname, "s.png"), frame: false, webPreferences: { nodeIntegration: true }, show: false, alwaysOnTop: true, minWidth: 1005, minHeight: 200 });
 
     splash = new BrowserWindow({
-        height: 310, width: 210, frame: false, alwaysOnTop: true, resizable: false, webPreferences: {
+        height: 310, width: 210, frame: false, alwaysOnTop: true, resizable: false, show: false, webPreferences: {
             nodeIntegration: true
         }
     });
+
+    splash.once('ready-to-show', function() {
+        splash.show();
+    });
+
     splash.loadURL(url.format({
         pathname: path.join(__dirname, '/dist/SoulsLauncher/splash.html'),
         protocol: 'file:',
@@ -49,7 +75,7 @@ app.on('ready', () => {
     }));
 
     autoUpdater.fullChangelog = false;
-    autoUpdater.checkForUpdates()
+//    autoUpdater.checkForUpdates()
 
     autoUpdater.on('update-available', function(info) {
         newVersion = true;
@@ -68,8 +94,9 @@ app.on('ready', () => {
             win.show();
             win.setAlwaysOnTop(false);
             win.focus();
-            if(newVersion == true) {
+            if(currentversion != maxversion && currentversionnumber < maxversionnumber) {
                 BrowserWindow.getFocusedWindow().webContents.send('update');
+                global.maxVersion = maxversion;
                 global.update = "true";
             }
         }, 1500);
@@ -78,11 +105,51 @@ app.on('ready', () => {
     win.on('closed', () => {
         win = null;
     });
+
+    ipcMain.on('downloadnewupdate', () => {
+        let focusedWindow = BrowserWindow.getFocusedWindow();
+        focusedWindow.webContents.send('downloadingupdate');
+        try {
+        const file = fs.createWriteStream("updates/update.zip");
+        const request = http.get('http://github.com/Kakebiten/soulslauncher/releases/download/v0.0.3-alpha/update.zip', function(response) {
+            response.pipe(file);
+            file.on('finish', function() {
+                file.close();
+                win.focus();
+                BrowserWindow.getFocusedWindow();
+                focusedWindow.webContents.send('updateDownloaded');
+            }).on('error', function(err) {
+                console.log(err);
+            });
+            
+        });
+        }catch(err) {
+            console.log(err);
+            electronLogger.error(err);
+        }
+    });
+
+    ipcMain.on('updateinstall', function() {
+        child_process.exec('node update.js', function(error, stdout, stderr) {
+            console.log(`${stdout}`);
+            console.log(`${stderr}`);
+            if(error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+            app.relaunch();
+            app.quit();
+        });
+    });
 });
 
 app.on('activate', () => {
     if (win == null)
     createWindow()
+});
+
+process.on('uncaughtException', (err) => {
+    console.log(err);
+    app.quit();
 });
 
 app.on('window-all-closed', () => {
